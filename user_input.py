ddae71a542e8f194117e17133fecdefb090ac9ff
Ogola Sospeter <okongo.ogola1@students.jkuat.ac.ke>
2024-08-21 12:17:34 +0300

    Fetching the user input

---
+++ user_input.py
@@ -0,0 +1,11 @@
+""""
+Use the input() function to ask the user for their name and store it in a variable called "name".
+Use the input() function to ask the user for their age and store it in a variable called "age".
+Use the input() function to ask the user for their location and store it in a variable called "location".
+Print out a personalized message using the user's name, age, and location. For example: "Hello [name], you are [age] years old and live in [location]."
+"""
+user_name = input("What is your name? ")
+user_age = input("What is your age? ")
+user_location = input("Where do you live? ")
+
+print(f"Hello {user_name}, you are {user_age} years old and live in {user_location}.")
